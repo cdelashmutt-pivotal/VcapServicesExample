c6fb76cd81f9aba7205f32c8967c932ffd3e9a23
Chris DeLashmutt <cdelashmutt@pivotal.io>
2017-06-28 11:39:07 -0400

    Initial

---
+++ VcapServicesExample/App_Start/ServerConfig.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+using Steeltoe.Extensions.Configuration.CloudFoundry;
+using PA = Microsoft.Extensions.PlatformAbstractions;
+
+namespace VcapServicesExample
+{
+    public class ServerConfig
+    {
+        public static CloudFoundryApplicationOptions CloudFoundryApplication
+        {
+
+            get
+            {
+                var opts = new CloudFoundryApplicationOptions();
+                ConfigurationBinder.Bind(Configuration, opts);
+                return opts;
+            }
+        }
+        public static CloudFoundryServicesOptions CloudFoundryServices
+        {
+            get
+            {
+                var opts = new CloudFoundryServicesOptions();
+                ConfigurationBinder.Bind(Configuration, opts);
+                return opts;
+            }
+        }
+
+        public static IConfigurationRoot Configuration { get; set; }
+
+        public static void RegisterConfig(string environment)
+        {
+            var env = new HostingEnvironment(environment);
+
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                //.AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+                //.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+
+        }
+    }
+    public class HostingEnvironment : IHostingEnvironment
+    {
+        public HostingEnvironment(string env)
+        {
+            EnvironmentName = env;
+            ApplicationName = PA.PlatformServices.Default.Application.ApplicationName;
+            ContentRootPath = PA.PlatformServices.Default.Application.ApplicationBasePath;
+        }
+
+        public string ApplicationName { get; set; }
+
+        public IFileProvider ContentRootFileProvider { get; set; }
+
+        public string ContentRootPath { get; set; }
+
+        public string EnvironmentName { get; set; }
+
+        public IFileProvider WebRootFileProvider { get; set; }
+
+        public string WebRootPath { get; set; }
+
+        IFileProvider IHostingEnvironment.WebRootFileProvider { get; set; }
+    }
+
+}
---
+++ VcapServicesExample/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Steeltoe.Extensions.Configuration.CloudFoundry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VcapServicesExample.ViewModel;
+
+namespace VcapServicesExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult CloudFoundry()
+        {
+
+            var cloudFoundryApplication = ServerConfig.CloudFoundryApplication;
+            var cloudFoundryServices = ServerConfig.CloudFoundryServices;
+            return View(new CloudFoundryViewModel(
+                cloudFoundryApplication == null ? new CloudFoundryApplicationOptions() : cloudFoundryApplication,
+                cloudFoundryServices == null ? new CloudFoundryServicesOptions() : cloudFoundryServices));
+        }
+    }
+}
